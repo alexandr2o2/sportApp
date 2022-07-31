b69ba2c5daf4f3ad5c0ef20af7067dbb0d7986fb
alexandr2o2 <aaz75@tpu.ru>
2022-07-31 11:34:33 +0600

    Initial commit

---
+++ src/main/java/ru/alexandr/sportapp/SportAppApplication.java
@@ -0,0 +1,14 @@
+package ru.alexandr.sportapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportAppApplication.class, args);
+        System.out.println("lol");
+    }
+
+}
